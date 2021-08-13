b3e27d6da712d384a3f07ef63714a9dffc099255
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2021-08-13 19:22:38 +0200

    0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ struct PluginInfo {
 
 static PLUGIN_INFO: PluginInfo = PluginInfo {
     name: static_cstr!("GC Adapter (for Wii U or Switch) Input Plugin"),
-    version: 0x000401,            // v0.4.1
+    version: 0x000500,            // v0.5.0
     target_api_version: 0x020100, // v2.1.0
 };
 
